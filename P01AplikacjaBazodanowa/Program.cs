fbf8dbf4f9737f85a3d323a53084fd37469c371a
Tomasz Leś <szkolenia@tomaszles.pl>
2024-02-02 09:21:01 +0100

    Add project files.

---
+++ P01AplikacjaBazodanowa/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P01AplikacjaBazodanowa
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SqlConnection connection; // nawiazywanie polaczenia z baza 
+            SqlCommand command; // przechowywanie polecen SQL 
+            SqlDataReader sqlDataReader; // czytanie wyników z bazy
+
+            string connectionString = "Data Source=(localdb)\\mssqllocaldb;Initial Catalog=A_Zawodnicy;Integrated Security=True";
+            connection = new SqlConnection(connectionString);
+
+            command = new SqlCommand("select * from zawodnicy", connection);
+
+            connection.Open();
+
+            sqlDataReader = command.ExecuteReader();
+
+            ////teraz musimy przeczytac wynik 
+            //sqlDataReader.Read(); // czytamy kolejny wiersz 
+            //string wynik = (string)sqlDataReader.GetValue(2); // pobierz wartosc z pierwszego wiersza i kolumny o indeksie 2 
+            //Console.WriteLine(wynik);
+
+            //sqlDataReader.Read(); // czytamy kolejny wiersz 
+            //wynik = (string)sqlDataReader.GetValue(2); // pobierz wartosc z pierwszego wiersza i kolumny o indeksie 2 
+            //Console.WriteLine(wynik);
+
+            while (sqlDataReader.Read())
+            {
+                string wynik = (string)sqlDataReader.GetValue(2) + " " + (string)sqlDataReader.GetValue(3);
+                Console.WriteLine(wynik);
+            }
+
+            connection.Close();
+
+            Console.ReadKey();
+
+        }
+    }
+}
